ca0032e0782cddb4ac0d02e317d61e05e379a63d
Jasmine Chen <jasmine1226@gmail.com>
2019-05-10 06:30:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,8 @@ def reverse_each_word(string)
   array = string.split(" ")
   reversed_string = ""
   array.each do |word|
-    #if array.length > 1
-    #  reversed_string = reversed_array + " "
-    #end
     reversed_string = reversed_string + word.reverse
+    puts array.length
     puts reversed_string
   end
   return reversed_string
