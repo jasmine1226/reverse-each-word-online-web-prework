6ef79c968b72d0b661975539b9479081c10418b0
Jasmine Chen <jasmine1226@gmail.com>
2019-05-10 06:21:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   reversed_array = ""
   array.each do |word|
-    reversed_array[array.legnth-1] = word.reverse
+    reversed_array = reversed_array + " " + word.reverse
   end
   return reversed_array
 end
