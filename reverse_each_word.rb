5b04876ad7bc702965c298c24c43a7115218e99d
Jasmine Chen <jasmine1226@gmail.com>
2019-05-10 06:33:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   reversed_string = ""
   array.each do |word|
     reversed_string = reversed_string + word.reverse
-    if array.index(word)-1 < array.length
+    if array.index(word)+1 < array.length
       reversed_string = reversed_string + " "
     end
     puts reversed_string
